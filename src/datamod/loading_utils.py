d7e4d8d9cd05b04c16dc19ab4603cd49f5564dcb
FKamper <francois.kamper@gmail.com>
2025-07-03 13:41:39 +0200

    changed argument overlap to stride in datamod

--- src/datamod/loading_utils.py
+++ src/datamod/loading_utils.py
@@ -2,7 +2,7 @@ import numpy as np
 from datetime import timedelta
 
 
-def sliding_windows_from_trace(inp, window_size=10000, overlap=5000):
+def sliding_windows_from_trace(inp, window_size=10000, stride=5000):
     start_time = inp.stats.starttime
     sr = inp.stats.sampling_rate
 
@@ -11,12 +11,12 @@ def sliding_windows_from_trace(inp, window_size=10000, overlap=5000):
     if inp.shape[0] < window_size:
         return np.zeros([0, window_size]), []
 
-    up = int((inp.shape[0] - window_size) / overlap) + 1
+    up = int((inp.shape[0] - window_size) / stride) + 1
     X = np.zeros([up, window_size])
     T = []
 
     for i in range(up):
-        t0 = overlap * i
+        t0 = stride * i
         t1 = t0 + window_size
         X[i, :] = inp[t0:t1]
         T.append(
@@ -29,14 +29,12 @@ def sliding_windows_from_trace(inp, window_size=10000, overlap=5000):
     return X, T
 
 
-def sliding_windows_from_stream(st, window_size=10000, overlap=5000):
+def sliding_windows_from_stream(st, window_size=10000, stride=5000):
     X = np.zeros([0, window_size])
     T = []
 
     for tr in st:
-        Xn, Tn = sliding_windows_from_trace(
-            tr, window_size=window_size, overlap=overlap
-        )
+        Xn, Tn = sliding_windows_from_trace(tr, window_size=window_size, stride=stride)
         if Xn.shape[0] == 0:
             continue
         T.append(Tn)
